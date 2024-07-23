5bfc38f96c36d7aff2883f53fc153b28d519c29b
fatahshireishi <fatah.shireishi@gmail.com>
2024-07-23 19:51:28 +0700

    fix package

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -70,39 +70,43 @@ const SearchHistoryPage = () => {
       <div className="max-w-6xl mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8">Search History</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {searchHistory.map((item) => (
-            <div
-              className="bg-gray-800 p-4 rounded flex items-start"
-              key={item.id}
-            >
-              <img
-                src={SMALL_IMG_BASE_URL + item.image}
-                alt="History Image"
-                className="size-16 rounded-full object-cover mr-4"
-              />
-              <div className="flex flex-col *:">
-                <span className="text-white text-lg">{item.title}</span>
-                <span className="text-gray-400 text-sm">
-                  {formatDate(item.createdAt)}
-                </span>
-              </div>
-              <span
-                className={`py-1 px-3 min-w-20 text-center rounded-full text-sm ml-auto ${
-                  item.searchType === "movie"
-                    ? "bg-red-600"
-                    : item.searchType === "tv"
-                    ? "bg-blue-600"
-                    : "bg-green-600"
-                }`}
+          {searchHistory.map((item) => {
+            if (item.image === null) return null;
+
+            return (
+              <div
+                className="bg-gray-800 p-4 rounded flex items-start"
+                key={item.id}
               >
-                {item.searchType[0].toUpperCase() + item.searchType.slice(1)}
-              </span>
-              <Trash
-                className="size-5 ml-4 cursor-pointer hover:fill-red-600 hover:text-red-600"
-                onClick={() => handleDelete(item)}
-              />
-            </div>
-          ))}
+                <img
+                  src={SMALL_IMG_BASE_URL + item.image}
+                  alt="History Image"
+                  className="size-16 rounded-full object-cover mr-4"
+                />
+                <div className="flex flex-col *:">
+                  <span className="text-white text-lg">{item.title}</span>
+                  <span className="text-gray-400 text-sm">
+                    {formatDate(item.createdAt)}
+                  </span>
+                </div>
+                <span
+                  className={`py-1 px-3 min-w-20 text-center rounded-full text-sm ml-auto ${
+                    item.searchType === "movie"
+                      ? "bg-red-600"
+                      : item.searchType === "tv"
+                      ? "bg-blue-600"
+                      : "bg-green-600"
+                  }`}
+                >
+                  {item.searchType[0].toUpperCase() + item.searchType.slice(1)}
+                </span>
+                <Trash
+                  className="size-5 ml-4 cursor-pointer hover:fill-red-600 hover:text-red-600"
+                  onClick={() => handleDelete(item)}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
