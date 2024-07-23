49bca0d08ae77225b808d183dacbfa919a59c32d
fatahshireishi <fatah.shireishi@gmail.com>
2024-07-23 23:30:24 +0700

    fix api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:5000",
         target: "https://netflix-clone-api-six.vercel.app",
         changeOrigin: true,
       },
