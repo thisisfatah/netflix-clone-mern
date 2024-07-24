6232ef937bc58bb5b4f719fb1ba5d7799e52183e
fatahshireishi <fatah.shireishi@gmail.com>
2024-07-24 09:51:27 +0700

    tes

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -22,8 +22,8 @@ export const useAuthStore = create((set) => ({
   login: async (credentials) => {
     set({ isLoggingIn: true });
     try {
-      const response = await axios.get(
-        "https://netflix-clone-api-six.vercel.app/api/v1/auth/getuser",
+      const response = await axios.post(
+        "https://netflix-clone-api-six.vercel.app/api/v1/auth/login",
         credentials
       );
       set({ user: response.data.user, isLoggingIn: false });
